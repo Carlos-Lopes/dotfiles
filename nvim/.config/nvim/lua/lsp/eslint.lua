0b2dfbfccc9d09e0610257b506f32d9589f0f9f8
Carlos Lopes <carloshenriquelopes@Mac-mini.local>
2025-10-27 18:56:38 -0300

    fix: eslint lsp execution

--- nvim/.config/nvim/lua/lsp/eslint.lua
+++ nvim/.config/nvim/lua/lsp/eslint.lua
@@ -13,14 +13,109 @@ vim.lsp.config("eslint", {
     "htmlangular",
   },
   workspace_required = true,
-  root_markers = { ".eslintrc.js", ".eslintrc.cjs", ".eslintrc.json", ".git" },
-  -- on_attach = function(client, bufnr)
-  --   vim.api.nvim_create_autocmd("BufWritePre", {
-  --     buffer = bufnr,
-  --     command = "LspEslintFixAll",
-  --   })
-  -- end,
+  root_markers = {
+    ".eslintrc",
+    ".eslintrc.js",
+    ".eslintrc.cjs",
+    ".eslintrc.yaml",
+    ".eslintrc.yml",
+    ".eslintrc.json",
+    "eslint.config.js",
+    "eslint.config.mjs",
+    "eslint.config.cjs",
+    "eslint.config.ts",
+    "eslint.config.mts",
+    "eslint.config.cts",
+  },
+  root_dir = function(bufnr, on_dir)
+    -- The project root is where the LSP can be started from
+    -- As stated in the documentation above, this LSP supports monorepos and simple projects.
+    -- We select then from the project root, which is identified by the presence of a package
+    -- manager lock file.
+    local root_markers = { "package-lock.json", "yarn.lock", "pnpm-lock.yaml", "bun.lockb", "bun.lock" }
+    local project_root = vim.fs.root(bufnr, root_markers) or vim.fn.getcwd()
+
+    on_dir(project_root)
+  end,
+  on_attach = function(client, bufnr)
+    -- Creates a buffer-local command `:LspEslintFixAll` that tells the ESLint LSP to
+    -- automatically apply all fixable linting errors to the current buffer
+    vim.api.nvim_buf_create_user_command(0, "LspEslintFixAll", function()
+      client:request_sync("workspace/executeCommand", {
+        command = "eslint.applyAllFixes",
+        arguments = {
+          { uri = vim.uri_from_bufnr(bufnr), version = vim.lsp.util.buf_versions[bufnr] },
+        },
+      }, nil, bufnr)
+    end, {})
+
+    -- Automatically run `:LspEslintFixAll` to fix all ESLint issues
+    -- before saving the current buffer.
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      callback = function()
+        vim.cmd("LspEslintFixAll")
+      end,
+    })
+  end,
+  -- Refer to https://github.com/Microsoft/vscode-eslint#settings-options for documentation.
+  settings = {
+    validate = "on",
+    packageManager = nil,
+    useESLintClass = false,
+    experimental = {
+      useFlatConfig = false,
+    },
+    codeActionOnSave = {
+      enable = false,
+      mode = "all",
+    },
+    format = true,
+    quiet = false,
+    onIgnoredFiles = "off",
+    rulesCustomizations = {},
+    run = "onType",
+    problems = {
+      shortenToSingleLine = false,
+    },
+    -- nodePath configures the directory in which the eslint server should start its node_modules resolution.
+    -- This path is relative to the workspace folder (root dir) of the server instance.
+    nodePath = "",
+    -- use the workspace folder location or the file location (if no workspace folder is open) as the working directory
+    workingDirectory = { mode = "auto" },
+    codeAction = {
+      disableRuleComment = {
+        enable = true,
+        location = "separateLine",
+      },
+      showDocumentation = {
+        enable = true,
+      },
+    },
+  },
+  handlers = {
+    ["eslint/openDoc"] = function(_, result)
+      if result then
+        vim.ui.open(result.url)
+      end
+      return {}
+    end,
+    ["eslint/confirmESLintExecution"] = function(_, result)
+      if not result then
+        return
+      end
+      return 4 -- approved
+    end,
+    ["eslint/probeFailed"] = function()
+      vim.notify("[lspconfig] ESLint probe failed.", vim.log.levels.WARN)
+      return {}
+    end,
+    ["eslint/noLibrary"] = function()
+      vim.notify("[lspconfig] Unable to find ESLint library.", vim.log.levels.WARN)
+      return {}
+    end,
+  },
 })
 
 -- Enable ESLint LSP for matching buffers
--- vim.lsp.enable("eslint")
+vim.lsp.enable("eslint")
